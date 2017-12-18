c533dc4ae66966b1a556a46a6c26167bb3c9346f
Matt Palamos <mattpalamos@hotmail.com>
2017-12-18 15:33:04 -0800

    added Header with font

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,16 @@
 // let's go!
+import React from 'react';
+//react comes from node modules folder
+import {render} from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+
+//import components
+import Main from './components/Main';
+
+
+
+render(<Main/>, document.getElementById('root'));
+
